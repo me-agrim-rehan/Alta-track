bbc617171e0f637d006f6ba282a8b8592485f0d7
me-agrim-rehan <agrimrehan.work@gmail.com>
2026-09-19 23:58:12 +0530

    meow

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -246,7 +246,7 @@ export async function login(req, res) {
     res.cookie("token", token, {
       httpOnly: true,
       secure: isProduction,
-      sameSite: isProduction ? "none" : "lax",
+      sameSite: "none" ,
       maxAge: 7 * 24 * 60 * 60 * 1000,
       path: "/",
     });
--- Frontend/api.js
+++ Frontend/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL = "https://alta-track.onrender.com";
 
 export async function loginUser(credentials) {
   const response = await fetch(`${API_BASE_URL}/auth/login`, {
